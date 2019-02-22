263d0db40cacd71b6b8a447ec3e619e53a17510b
Stuart Gill <stuart.a.gill@gmail.com>
2019-02-22 09:05:23 -0800

    add linked in and github to contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,10 +3,22 @@ import React from "react";
 function Contact() {
   return (
     <div>
-      <h2>I'm available for work, get in touch</h2>
+      <h2>I'm available for work, get in touch!</h2>
       <p>
-        email:{" "}
+        Email:{"  "}
         <a href="mailto: stuart.a.gill@gmail.com">stuart.a.gill@gmail.com</a>
+      </p>
+      <p>
+        Linked in:{" "}
+        <a href="https://www.linkedin.com/in/stuart-gill">
+          https://www.linkedin.com/in/stuart-gill
+        </a>
+      </p>
+      <p>
+        Github:{" "}
+        <a href="https://github.com/stuart-gill">
+          https://github.com/stuart-gill
+        </a>
       </p>
     </div>
   );
