0aca9c66b73570b6e3e0a292745733211111f93f
Stuart Gill <stuart.a.gill@gmail.com>
2019-02-22 09:19:30 -0800

    change app component to stateful class based component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,29 +7,35 @@ import PortfolioOfProjects from "./PortfolioOfProjects";
 import About from "./About";
 import Contact from "./Contact";
 
-function App() {
-  return (
-    <div>
-      <style jsx global>
-        {`
-          body {
-            font-family: "Poppins", sans-serif;
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-            background-image: url("https://s3-us-west-2.amazonaws.com/unseenworldsrecords/uploads/setting/background_image/5/Untitled-1.png");
-          }
-        `}
-      </style>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/portfolio" component={PortfolioOfProjects} />
+  render() {
+    return (
+      <div>
+        <style jsx global>
+          {`
+            body {
+              font-family: "Poppins", sans-serif;
 
-        <Route exact path="/about" component={About} />
-        <Route exact path="/contact" component={Contact} />
-        <Route component={Error404} />
-      </Switch>
-    </div>
-  );
+              background-image: url("https://s3-us-west-2.amazonaws.com/unseenworldsrecords/uploads/setting/background_image/5/Untitled-1.png");
+            }
+          `}
+        </style>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/portfolio" component={PortfolioOfProjects} />
+
+          <Route exact path="/about" component={About} />
+          <Route exact path="/contact" component={Contact} />
+          <Route component={Error404} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
